37a7444cb1387d26e6ea797f3949696d0e08278d
Oscar131-maker <oscarweb01@gmail.com>
2020-02-27 18:15:17 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const FullPage = new fullpage('#fullpage', {
+	//options here
+	autoScrolling:false,
+	scrollHorizontally: true,
+
+
+	navigation:true,
+	menu:'#menu',
+	anchors: ['home', 'aboutMe', 'briefcase', 'contact'],
+
+});
